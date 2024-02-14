5dada32f82b9ec7fcc986fdfc6a4a2a055941e5b
Alex Kadai <alexandrkadai@gmail.com>
2024-02-14 21:39:06 +0200

    NAvbar auto close

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,14 +1,21 @@
 'USE CLIENT';
 import { PRODUCT_CATEGORIES } from '@/config';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import NavItem from './NavItem';
+import { useOnClickOutside } from '@/hooks/useOnClickOutside';
+
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
   const isAnyOpen = activeIndex !== null;
 
+  const navRef = useRef<HTMLDivElement | null>(null);
+  useOnClickOutside(navRef, () => {
+    setActiveIndex(null);
+  });
+
   return (
-    <div className="flex gap-4 h-full">
+    <div className="flex gap-4 h-full" ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, i) => {
         const handleOpen = () => {
           if (activeIndex === i) {
@@ -22,7 +29,13 @@ const NavItems = () => {
 
         return (
           <>
-            <NavItem category={category} handleOpen={handleOpen} isOpen={isOpen} key={category.value} isAnyOpen={isAnyOpen}/>
+            <NavItem
+              category={category}
+              handleOpen={handleOpen}
+              isOpen={isOpen}
+              key={category.value}
+              isAnyOpen={isAnyOpen}
+            />
           </>
         );
       })}
