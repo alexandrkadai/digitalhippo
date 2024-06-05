e979c28cd163da0ea705ca50257d7480e331122e
Alex Kadai <alexandrkadai@gmail.com>
2024-06-05 11:57:18 +0300

    Payload config and server

--- src/getPayLoad.ts
+++ src/getPayLoad.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import path from 'path';
 import type { InitOptions } from 'payload/config';
+import payload from 'payload';
 
 dotenv.config({
   path: path.resolve(__dirname, '../.env'),
@@ -14,12 +15,30 @@ if (!cashed) {
     promise: null,
   };
 }
-interface Args{
-    initOption?: Partial<InitOptions> 
-        }   ;
+interface Args {
+  initOptions?: Partial<InitOptions>;
+}
 
-export const getPayloadClient = async ({initOption} : Args = {} ) => {
-    if(!process.env.PAYLOAD_SECRET ){
-        throw new Error(' The PAYLOAD SECRET is Missing');
-    }
+export const getPayloadClient = async ({ initOptions }: Args = {}) => {
+  if (!process.env.PAYLOAD_SECRET) {
+    throw new Error(' The PAYLOAD SECRET is Missing');
+  }
+  if (cashed.client) {
+    return cashed.client;
+  }
+  if (!cashed.promise) {
+    cashed.promise = payload.init({
+      secret: process.env.PAYLOAD_SECRET,
+      local: initOptions?.express ? false : true,
+      ...(initOptions || {}),
+    });
+  }
+
+  try {
+    cashed.client = await cashed.promise;
+  } catch (error: unknown) {
+    cashed.promise = null;
+    throw error;
+  }
+  return cashed.client;
 };
